9a4900d48d42e2f6c118a5ca85015a10436f4145
Tommaso Dario Danti <tommasodanti@yahoo.it>
2024-01-26 16:33:41 +0100

    refactor: Remove index.css import, moved root.tsx page to screens dir and add root.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./index.css";
 import { router } from "./router/router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
