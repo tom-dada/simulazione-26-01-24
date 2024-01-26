6d2cc6c1ad1e974102c5ca2432b41559c9d6a452
Tommaso Dario Danti <tommasodanti@yahoo.it>
2024-01-26 12:34:13 +0100

    fix(API): fix getMovies param

--- src/api/omdb.ts
+++ src/api/omdb.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function getMovies() {
   try {
     const response = await axios.get(
-      "http://www.omdbapi.com/?apikey=[d572c55e]&s=''"
+      "http://www.omdbapi.com/?apikey=[d572c55e]&s=batman"
     );
     console.log(response);
   } catch (error) {
