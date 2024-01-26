804ef7ffc8c7db63ac1a32f060fb3c29c6fee688
Tommaso Dario Danti <tommasodanti@yahoo.it>
2024-01-26 16:37:08 +0100

    feat(router): Update router configuration

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../screens/error-page";
-import Root from "../screens/root";
+import Root from "../screens/root/root";
+import Movies from "../screens/movies";
+import { movieLoader } from "../screens/movies";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: movieLoader,
+    children: [
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+    ],
   },
 ]);
